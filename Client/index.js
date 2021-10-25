9422f69996423a3bac1d616aa1054bd21f95ea72
dunguyen-3112 <79679651+dunguyen-3112@users.noreply.github.com>
2021-10-25 09:09:26 +0700

    add client

---
+++ Client/index.js
@@ -0,0 +1,20 @@
+const ipcRenderer = require('electron').ipcRenderer;
+
+window.onload = function() {
+    ipcRenderer.on("uuid", (event, data) => {
+        document.getElementById("code").innerHTML = data;
+    })
+}
+
+function startShare() {
+    ipcRenderer.send("start-share", {});
+    document.getElementById("start").style.display = "none";
+    document.getElementById("stop").style.display = "block";
+}
+
+function stopShare() {
+    ipcRenderer.send("stop-share", {});
+    document.getElementById("stop").style.display = "none";
+    document.getElementById("code").innerHTML = '';
+    document.getElementById("start").style.display = "block";
+}
